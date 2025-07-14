3fa6311008885f61b0db29187712e8216d1fb53f
quan-nguyen18 <quan.nguyennguyen@hcmut.edu.vn>
2025-07-14 21:53:45 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,47 @@
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
-let originalImageData = null;
-let currentImage = new Image();
+let originalImage = null;
 
-document.getElementById("upload").addEventListener("change", function (e) {
-  const file = e.target.files[0];
-  if (!file) return;
-
-  const reader = new FileReader();
-  reader.onload = function (event) {
-    currentImage = new Image();
-    currentImage.onload = function () {
-      canvas.width = currentImage.width;
-      canvas.height = currentImage.height;
-      ctx.drawImage(currentImage, 0, 0);
-      originalImageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-    };
-    currentImage.src = event.target.result;
+document.getElementById("file-input").addEventListener("change", (e) => {
+  let file = e.target.files[0];
+  let img = new Image();
+  img.onload = () => {
+    canvas.width = img.width;
+    canvas.height = img.height;
+    ctx.drawImage(img, 0, 0);
+    originalImage = ctx.getImageData(0, 0, canvas.width, canvas.height);
   };
-  reader.readAsDataURL(file);
+  img.src = URL.createObjectURL(file);
 });
+
+function resetImage() {
+  if (originalImage) {
+    ctx.putImageData(originalImage, 0, 0);
+  }
+}
+
+function applyGrayscale() {
+  let imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+  let data = imgData.data;
+  for (let i = 0; i < data.length; i += 4) {
+    let avg = (data[i] + data[i+1] + data[i+2]) / 3;
+    data[i] = data[i+1] = data[i+2] = avg;
+  }
+  ctx.putImageData(imgData, 0, 0);
+}
+
+function applySobelX() {
+  alert("Sobel X not implemented yet.");
+}
+
+function applySobelY() {
+  alert("Sobel Y not implemented yet.");
+}
+
+function rotateImage() {
+  alert("Rotate not implemented yet.");
+}
+
+function cropImage() {
+  alert("Crop not implemented yet.");
+}
